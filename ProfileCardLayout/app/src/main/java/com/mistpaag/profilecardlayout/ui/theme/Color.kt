8e42aabaae23ece84424978cdcdebbc079994a26
Michael Pardo <michael.stiver.pardo@gmail.com>
2021-11-17 11:26:10 -0500

    customizing colors in theme

---
+++ ProfileCardLayout/app/src/main/java/com/mistpaag/profilecardlayout/ui/theme/Color.kt
@@ -0,0 +1,17 @@
+package com.mistpaag.profilecardlayout.ui.theme
+
+import androidx.compose.material.Colors
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
+
+val veryLightGrey = Color(0x60DCDCDC)
+val lightGreen200 = Color(0x9932CD32)
+
+val Colors.lightGreen: Color
+    @Composable
+    get() = lightGreen200
