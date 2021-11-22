e8327b933c2168528276c10b96ed50ad6ec26e88
Michael Pardo <michael.stiver.pardo@gmail.com>
2021-11-22 17:39:10 -0500

    callback for network response

--- MealzApp/app/src/main/java/com/mistpaag/mealzapp/ui/meals/MainActivity.kt
+++ MealzApp/app/src/main/java/com/mistpaag/mealzapp/ui/meals/MainActivity.kt
@@ -4,10 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.mistpaag.mealzapp.model.response.MealResponse
 import com.mistpaag.mealzapp.ui.theme.MealzAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -24,8 +29,17 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MealsCategoriesScreen() {
     val viewModel : MealsCategoriesViewModel = viewModel()
-    val meals = viewModel.getMeals()
-    Text(text = meals.toString())
+    val rememberedMeals = remember { mutableStateOf(emptyList<MealResponse>()) }
+    viewModel.getMeals { response ->
+        val mealsFromTheApi = response?.categories
+        rememberedMeals.value = mealsFromTheApi.orEmpty()
+    }
+    LazyColumn {
+        items(rememberedMeals.value) { meal ->
+            Text(text = meal.name)
+        }
+    }
+
 }
 
 @Preview(showBackground = true)
--- MealzApp/app/src/main/java/com/mistpaag/mealzapp/ui/meals/MealsCategoriesViewModel.kt
+++ MealzApp/app/src/main/java/com/mistpaag/mealzapp/ui/meals/MealsCategoriesViewModel.kt
@@ -3,14 +3,17 @@ package com.mistpaag.mealzapp.ui.meals
 import androidx.lifecycle.ViewModel
 import com.mistpaag.mealzapp.model.MealsRepository
 import com.mistpaag.mealzapp.model.response.MealResponse
+import com.mistpaag.mealzapp.model.response.MealsCategoriesResponse
 
 class MealsCategoriesViewModel(
     private val repository: MealsRepository = MealsRepository()
 ): ViewModel() {
 
 
-    fun getMeals() : List<MealResponse> {
-        return repository.getMeals()?.categories.orEmpty()
+    fun getMeals(successCallback: (response: MealsCategoriesResponse?) -> Unit) {
+        return repository.getMeals{
+            successCallback(it)
+        }
     }
 
 }
