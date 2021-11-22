e7f9882ccc0a41da4dd4e8611aae7f7de6d06677
Michael Pardo <michael.stiver.pardo@gmail.com>
2021-11-22 16:58:18 -0500

    structuring the response classes

--- MealzApp/app/src/main/java/com/mistpaag/mealzapp/model/MealsRepository.kt
+++ MealzApp/app/src/main/java/com/mistpaag/mealzapp/model/MealsRepository.kt
@@ -1,5 +1,8 @@
 package com.mistpaag.mealzapp.model
 
+import com.mistpaag.mealzapp.model.response.MealsCategoriesResponse
+
 class MealsRepository {
 
+    fun getMeals(): MealsCategoriesResponse = MealsCategoriesResponse(arrayListOf())
 }
--- MealzApp/app/src/main/java/com/mistpaag/mealzapp/ui/meals/MealsCategoriesViewModel.kt
+++ MealzApp/app/src/main/java/com/mistpaag/mealzapp/ui/meals/MealsCategoriesViewModel.kt
@@ -2,11 +2,15 @@ package com.mistpaag.mealzapp.ui.meals
 
 import androidx.lifecycle.ViewModel
 import com.mistpaag.mealzapp.model.MealsRepository
+import com.mistpaag.mealzapp.model.response.MealResponse
 
 class MealsCategoriesViewModel(
     private val repository: MealsRepository = MealsRepository()
 ): ViewModel() {
 
 
+    fun getMeals() : List<MealResponse> {
+        return repository.getMeals().categories
+    }
 
 }
